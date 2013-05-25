fc7e6b83321a899b38da59acf8d922eb91d82cf1
Evan Goodberry <egoodberry@gmail.com>
2013-05-25 16:19:36 -0400

    Whoops- actually removing.

--- wp-content/themes/diner/js/nav.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  App.setUpNav = function() {
-    // smooth scrolling nav
-    $("#header nav").visualNav({
-      selectedAppliedTo: "a",
-      selectedClass: "current"
-    });
-  }
-})();
