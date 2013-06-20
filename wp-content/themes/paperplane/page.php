d54f5a94f48ae051c6457785cccb9730e8ea4a43
Evan Goodberry <egoodberry@gmail.com>
2013-06-19 21:48:02 -0400

    Adds styles to reservations page.

--- wp-content/themes/paperplane/page.php
+++ wp-content/themes/paperplane/page.php
@@ -7,8 +7,13 @@
 <p><img src="<?php bloginfo('template_directory'); ?>/images/logo-big.png" alt="Paper Plane" /></p>
 
 
-<h1 class="reserve-header">
-  Reserve
-</h1>
+<div class="reserve-header">
+  <h1>
+    Reserve
+  </h1>
+  <div class="vr">&nbsp;</div>
+
+  <p>Some of our tables are available nightly for dinner reservations. Reservations are for dinner only. If the time you wish to dine is not unavailable, please note that we still accept walk-ins throughout the evening. If you are joining us for cocktails, the bar is first come, first served.</p>
+</div>
 
 <?php the_content(); ?>
